ae75e5f6148d4aae3e41e3494de91df8e816ffd9
Manny Adumbire <manny@devunstuck.com>
2023-09-28 10:42:04 -0400

    Restore logo img

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>Hello CSCI E-39!</p>
         <p>
           My name is Manny.
